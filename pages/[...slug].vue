914102b261799b64613206dfdd52a3beda8cf9f6
Marc Schneider <marc@unknown.digital>
2023-08-13 14:49:11 +0200

    Simply fetch data

---
+++ pages/[...slug].vue
@@ -0,0 +1,15 @@
+<script setup>
+const QUERY = `
+  query MyQuery {
+    test {
+      id
+    }
+  }
+`;
+const { data, error } = await useGraphqlQuery({ query: QUERY });
+</script>
+
+<template>
+  <p v-if="error">Something bad happened!</p>
+  <p v-else>Data: <code>{{ data }}</code></p>
+</template>
