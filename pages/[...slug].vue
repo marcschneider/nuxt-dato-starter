ebde015591699c5cb8780047da8e150933a307d0
Marc Schneider <marc@unknown.digital>
2023-09-05 17:15:01 +0200

    simple page query

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,15 +1,49 @@
 <script setup>
-const QUERY = `
-  query MyQuery {
-    test {
+function findPage(pages, params) {
+  const param = params.shift();
+  const page = pages.find(p => p.slug === param);
+  if (!page) {
+    return null;
+  }
+  if (params.length === 0) {
+    return page;
+  }
+  return findPage(page.children, params);
+}
+
+const allPagesQuery = `
+  query ($hasParent: BooleanType) {
+    allPages(filter: {parent: {exists: $hasParent}}) {
       id
+      slug
+      pageName
+      children {
+        id
+        slug
+        children {
+          id
+          slug
+        }
+      }
     }
   }
 `;
-const { data, error } = await useGraphqlQuery({ query: QUERY });
+
+const { data, error } = await useGraphqlQuery({
+  query: allPagesQuery,
+  variables: {
+    hasParent: false,
+  },
+});
+
+// const routeArr = params.pathMatch.match(/([^\/]+)/g) || [''];
 </script>
 
 <template>
-  <p v-if="error">Something bad happened!</p>
-  <p v-else>Data: <code>{{ data }}</code></p>
+  <div v-if="error">Something bad happened!</div>
+  <div v-else>
+    <div v-for="page in data.allPages" :key="page.id">
+      {{ page.pageName }}
+    </div>
+  </div>
 </template>
