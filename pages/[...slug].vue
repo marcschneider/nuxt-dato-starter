d0a161c7bbf1d75b8766f8e5a3740c8794658fee
Marc Schneider <marc@unknown.digital>
2023-09-05 21:14:35 +0200

    span

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -43,9 +43,9 @@ const { data, error } = await useGraphqlQuery({
   <div v-if="error">Something bad happened!</div>
   <div class="m-2 flex flex-row w-full gap-2" v-else>
     <div v-for="page in data.allPages" :key="page.id">
-      <p class="cursor-pointer hover:bg-green-300 transition text-md px-4 py-1 bg-green-200 text-green-800 rounded-md font-medium">
+      <span class="cursor-pointer hover:bg-green-300 transition text-md px-4 py-1 bg-green-200 text-green-800 rounded-md font-medium">
         {{ page.pageName }}
-      </p>
+      </span>
     </div>
   </div>
 </template>
