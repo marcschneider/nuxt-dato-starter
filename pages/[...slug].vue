de6cc2467a15bf7b9d5ea29df34789db750603b6
Marc Schneider <marc@unknown.digital>
2023-09-05 22:05:45 +0200

    Start implementing elegant solutiomn

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,15 +1,19 @@
 <script setup>
-function findPage(pages, params) {
-  const param = params.shift();
-  const page = pages.find(p => p.slug === param);
-  if (!page) {
-    return null;
-  }
-  if (params.length === 0) {
-    return page;
-  }
-  return findPage(page.children, params);
-}
+
+// Steps
+// 1. Get full url
+// 2. Extract the last part of the url
+// 3. Make a query that searches for the last part of the url and load the id, slug, pageName, children
+// 4. Loop trough all results and check if the children slugs match the rest of the slugs
+// 5. If a result was found load the content of the page id
+
+
+const route = useRoute()
+console.log(route.path.match(/\/([^/]+)\/?$/)[1])
+
+
+
+/////////////////
 
 const allPagesQuery = `
   query ($hasParent: BooleanType) {
@@ -34,18 +38,19 @@ const { data, error } = await useGraphqlQuery({
   variables: {
     hasParent: false,
   },
-});
+})
 
-// const routeArr = params.pathMatch.match(/([^\/]+)/g) || [''];
 </script>
 
 <template>
   <div v-if="error">Something bad happened!</div>
   <div class="m-2 flex flex-row w-full gap-2" v-else>
-    <div v-for="page in data.allPages" :key="page.id">
-      <span class="cursor-pointer hover:bg-green-300 transition text-md px-4 py-1 bg-green-200 text-green-800 rounded-md font-medium">
-        {{ page.pageName }}
-      </span>
+    <div
+      class="cursor-pointer hover:bg-green-300 transition text-md px-4 py-1 bg-green-200 text-green-800 rounded-md font-medium"
+      v-for="page in data.allPages"
+      :key="page.id"
+    >
+      {{ page.pageName }}
     </div>
   </div>
 </template>
