9d1d1edb31e30dc156202840c198f8f90af0fce9
Marc Schneider <marc@unknown.digital>
2023-11-10 16:02:48 +0100

    fix if route is home

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,7 +1,12 @@
 <script setup>
 const route = useRoute()
 const routeParams = route.path.split('/').filter(Boolean)
-const lastRouteItem = routeParams[routeParams.length - 1]
+let lastRouteItem = routeParams[routeParams.length - 1]
+const isRootPage = routeParams.length === 0;
+
+if(isRootPage) {
+  lastRouteItem = '';
+}
 
 // Make a query that searches for the last part of the url and load the id, slug, pageName, parent
 const matchingSlugsQuery = `
