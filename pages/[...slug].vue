3eb477dd40ec7fd33f88eff764eeb9733d69df45
Marc Schneider <marc@unknown.digital>
2023-11-21 11:40:47 +0100

    Temporary disable

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -19,7 +19,8 @@ else {
 </script>
 
 <template>
-  <TemplateTeam v-if="slugType === 'specialSlugTeam'" :page-data="pageData.value.team" />
+  {{ pageData }}
+  <!-- <TemplateTeam v-if="slugType === 'specialSlugTeam'" :page-data="pageData.value.team" />
   <TemplateBlog v-else-if="slugType === 'specialSlugBlog'" :page-data="pageData.value.blog" />
-  <TemplateCommon v-else :page-data="pageData.value.page" />
+  <TemplateCommon v-else :page-data="pageData.value.page" /> -->
 </template>
