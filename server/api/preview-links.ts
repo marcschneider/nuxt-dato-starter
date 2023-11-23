f3ebbe2e4932e1040883798c3e20238656aecba4
Marc Schneider <marc@unknown.digital>
2023-11-23 16:06:11 +0100

    start implementing more dynamic preview links

--- server/api/preview-links.ts
+++ server/api/preview-links.ts
@@ -1,18 +1,47 @@
-import generateUrl from '~/composables/generateUrl'
-
-function generatePreviewUrl({ item, itemType, locale }) {
-  let url = null
-  switch (itemType.attributes.api_key) {
-    case 'page':
-      url = generateUrl()
-      return `page/${item.id}/${url}`
-    case 'blog':
-      return `blog/${item.id}`
-    case 'team':
-      return `team/${item.id}`
-    default:
-      return null
+const pageQuery = `
+  query Page($pageId: ItemId) {
+    page(filter: {id: {eq: $pageId}}) {
+      slug
+    }
   }
+`
+
+async function loadData(pageId) {
+  const data = await $fetch('https://graphql.datocms.com/', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer 04311cbb1d838ee1c833a3d20cdeb8`,
+    },
+    body: {
+      query: pageQuery,
+      variables: {
+        pageId,
+      },
+    },
+  })
+  return data || null
+}
+
+async function generatePreviewUrl() {
+  // const { item, itemType, locale } = body
+  let url = null
+  url = await loadData('94688351')
+  if (url)
+    return url
+  else
+    return null
+
+  // switch (itemType.attributes.api_key) {
+  //   case 'page':
+  //     url = generateUrl()
+  //     return `page/${item.id}/${url}`
+  //   case 'blog':
+  //     return `blog/${item.id}`
+  //   case 'team':
+  //     return `team/${item.id}`
+  //   default:
+  //     return null
+  // }
 }
 
 export default eventHandler(async (event) => {
@@ -22,10 +51,11 @@ export default eventHandler(async (event) => {
     'Access-Control-Allow-Methods': 'POST',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   })
+
   if (event.node.req.method === 'OPTIONS')
     return send(event, 'ok')
 
-  const url = generatePreviewUrl(await readBody(event))
+  const url = await generatePreviewUrl(await readBody(event))
 
   return {
     previewLinks: [
