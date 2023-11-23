e4ebe703269ca341651b7a4cc2d69b8f2c658845
Marc Schneider <marc@unknown.digital>
2023-11-23 18:43:58 +0100

    Styling

--- components/template/Blog.vue
+++ components/template/Blog.vue
@@ -21,11 +21,12 @@ function dynamicComponent(section) {
     <h1 class="text-4xl mb-8">
       {{ pageData.title }}
     </h1>
-    <div class="text-sm tracking-wide font-mono w-6/12 bg-green-100 p-6 rounded-xl mt-2">
+    <div>
       <component
         :is="dynamicComponent(section)"
         v-for="section in pageData.content"
         :key="section.id"
+        class="text-sm tracking-wide font-mono w-6/12 bg-green-100 p-6 rounded-xl mt-2"
         :section-data="section"
       />
     </div>
