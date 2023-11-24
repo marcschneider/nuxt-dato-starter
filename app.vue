145f1bfca6975a8224d0ce820145268ff35cd726
Marc Schneider <marc@unknown.digital>
2023-11-24 16:02:50 +0100

    Add costum font

--- app.vue
+++ app.vue
@@ -3,18 +3,3 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-
-<style>
-html {
-  @apply antialiased sc:text-[0.8333333333vw];
-}
-
-.page-enter-active,
-.page-leave-active {
-  @apply transition duration-500;
-}
-.page-enter-from,
-.page-leave-to {
-  @apply opacity-0 blur-[1rem];
-}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: [],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['GT Haptik'],
+      },
       screens: {
         ph: { max: '767px' },
         ta: { min: '768px', max: '1279px' },
