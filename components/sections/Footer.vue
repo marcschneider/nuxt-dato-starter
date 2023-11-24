a165ab0ace146448998d485a341e6c5370f26cc4
Marc Schneider <marc@unknown.digital>
2023-11-24 14:31:32 +0100

    styling

--- components/sections/Footer.vue
+++ components/sections/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sc:px-32 la:px-16 ta:px-6 ph:px-4 flex justify-between h-24 items-center">
+  <div class="sc:px-32 la:px-16 ta:px-6 ph:px-4 mt-10 flex justify-between h-24 items-center">
     <div>Copyright</div>
     <div>Links</div>
   </div>
