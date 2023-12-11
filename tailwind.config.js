743192939797b9b683e560327c1c64b29149170f
Marc Schneider <marc@unknown.digital>
2023-12-11 17:00:26 +0100

    Remove imported font as default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['GT Haptik', ...defaultTheme.fontFamily.sans],
+        haptik: ['GT Haptik', ...defaultTheme.fontFamily.sans],
       },
       screens: {
         phone: { max: '767px' },
