3c24f8d890f6ed50abf88a7d6fca8902c67c4e3f
Marc Schneider <marc@unknown.digital>
2023-12-11 16:34:07 +0100

    Make a nice login page

--- pages/preview.vue
+++ pages/preview.vue
@@ -24,21 +24,39 @@ function disablePreview() {
 
 <template>
   <div class="flex items-center justify-center h-screen">
-    <div v-if="!isPreview" class="w-64 p-6 bg-white rounded shadow-md">
+    <div v-if="!isPreview" class="p-6 bg-white rounded shadow-md w-72">
+      <p class="mb-4 text-lg font-bold">
+        Enable preview mode
+      </p>
       <form @submit.prevent="enablePreview">
-        <label for="password" class="sr-only">Password</label>
-        <input id="password" v-model="password" type="password" placeholder="Password" class="w-full px-3 py-2 mb-4 text-gray-900 placeholder-gray-500 rounded-lg focus:outline-none focus:shadow-outline-blue focus:border-blue-300">
-        <p v-if="errorMessage" class="text-red-500">
-          {{ errorMessage }}
-        </p>
-        <button type="submit" class="w-full px-3 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none">
-          Submit
+        <div class="mb-4">
+          <label for="password" class="sr-only">Password</label>
+          <input
+            id="password"
+            v-model="password"
+            type="password"
+            placeholder="Password"
+            class="w-full px-3 py-2 mb-2 text-black placeholder-gray-500 bg-gray-100 rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+          >
+          <p v-if="errorMessage" class="text-sm text-red-500">
+            {{ errorMessage }}
+          </p>
+        </div>
+        <button type="submit" class="w-full px-3 py-2 text-white bg-green-500 rounded hover:bg-green-600 focus:outline-none">
+          Enable
         </button>
       </form>
     </div>
-    <div v-else class="w-64 p-6 text-center bg-white rounded shadow-md">
-      <p>You are already logged in.</p>
-      <a class="underline" @click="disablePreview">Logout</a>
+    <div v-else class="p-6 bg-white rounded shadow-md w-72">
+      <p class="mb-4 text-lg font-bold">
+        Preview is enabled
+      </p>
+      <button
+        class="w-full px-3 py-2 text-white bg-green-500 rounded hover:bg-green-600 focus:outline-none"
+        @click="disablePreview"
+      >
+        Disable
+      </button>
     </div>
   </div>
 </template>
