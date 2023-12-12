dbdc75eeffced0a36073fac3a44f433d954abf5f
Marc Schneider <marc@unknown.digital>
2023-12-12 17:15:21 +0100

    Test

--- server/api/preview-links.js
+++ server/api/preview-links.js
@@ -59,8 +59,9 @@ async function generateRedirectUrl({ item, itemType }) {
 }
 
 export default eventHandler(async (event) => {
-  const requestURL = useRequestURL()
-  const hostname = requestURL.hostname
+  // const requestURL = useRequestURL()
+  // const hostname = requestURL.hostname
+  const hostname = 'https://develop--unknown-dato.netlify.app'
 
   setResponseHeaders(event, {
     'Content-Type': 'application/json',
